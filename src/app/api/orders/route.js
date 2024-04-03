df5618c62169a5ed72f739ee697726f59614594c
tusharrr622 <tusharraj622@gmail.com>
2024-04-03 17:03:29 +0530

    orders

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -7,41 +7,18 @@ import { UserInfo } from "@/models/UserInfo";
 
 
 export async function GET(req) {
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
+   await mongoose.connect(process.env.MONGO_URL);
 
-        const session = await getServerSession(authOptions);
-        const userEmail = session?.user?.email;
+    const session = await getServerSession(authOptions);
+    const userEmail = session?.user?.email;
 
-        const url = new URL(req.url);
-        const _id = url.searchParams.get('_id');
+    const url = new URL(req.url);
+    const _id = url.searchParams.get('_id');
+    if (_id) {
+        return Response.json(await Order.findById(_id));
+    }
 
-        const userInfo = await UserInfo.findOne({ email: userEmail });
-
-        if (_id) {
-            const order = await Order.findById(_id);
-            if (order) {
-                return Response.json(order);
-            } else {
-                return Response.status(404).json({ message: 'Order not found' });
-            }
-        }
-
-        if (userInfo && userInfo.admin) {
-            const orders = await Order.find();
-            return Response.json(orders);
-        }
-
-        if (userEmail) {
-            const userOrders = await Order.find({ userEmail });
-            return Response.json(userOrders);
-        }
-
-        return Response.status(403).json({ message: 'Unauthorized' });
-    } catch (error) {
-        console.error(error);
-        return Response.status(500).json({ message: 'Internal Server Error' });
-    } finally {
-        await mongoose.disconnect();
+    if (userEmail) {
+        return Response.json(await Order.find({ userEmail }));
     }
 }
