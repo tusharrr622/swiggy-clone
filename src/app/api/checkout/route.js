7bdb9f9dcf8ad7592751967b1e299af7f891e796
tusharrr622 <tusharraj622@gmail.com>
2024-04-04 21:03:18 +0530

    checkout up

--- src/app/api/checkout/route.js
+++ src/app/api/checkout/route.js
@@ -9,151 +9,79 @@ import { MenuItem } from "@/models/MenuItem";
 const stripe = require('stripe')(process.env.STRIPE_SK);
 
 export async function POST(req) {
-    //  await mongoose.connect(process.env.MONGO_URL);
+    mongoose.connect(process.env.MONGO_URL);
 
-    //     const { cartProducts, address } = await req.json();
-    //     const session = await getServerSession(authOptions);
-    //     const userEmail = session?.user?.email;
+    const { cartProducts, address } = await req.json();
+    const session = await getServerSession(authOptions);
+    const userEmail = session?.user?.email;
 
-    //     const orderDoc = await Order.create({
-    //         userEmail,
-    //         ...address,
-    //         cartProducts,
-    //         paid: false,
-    //     });
 
-    //     const stripeLineItems = [];
-    //     for (const cartProduct of cartProducts) {
+    if (mongoose.connection.readyState !== 1) {
+        throw new Error('MongoDB connection is not open');
+    }
 
-    //         const productInfo = await MenuItem.findById(cartProduct._id);
+    const orderDoc = await Order.create({
+        userEmail,
+        ...address,
+        cartProducts,
+        paid: false,
+    });
 
-    //         let productPrice = productInfo.basePrice;
-    //         if (cartProduct.size) {
-    //             const size = productInfo.sizes
-    //                 .find(size => size._id.toString() === cartProduct.size._id.toString());
-    //             productPrice += size.price;
-    //         }
-    //         if (cartProduct.extras?.length > 0) {
-    //             for (const cartProductExtraThing of cartProduct.extras) {
-    //                 const productExtras = productInfo.extraIngredientPrices;
-    //                 const extraThingInfo = productExtras
-    //                     .find(extra => extra._id.toString() === cartProductExtraThing._id.toString());
-    //                 productPrice += extraThingInfo.price;
-    //             }
-    //         }
+    const stripeLineItems = [];
+    for (const cartProduct of cartProducts) {
 
-    //         const productName = cartProduct.name;
+        const productInfo = await MenuItem.findById(cartProduct._id);
 
-    //         stripeLineItems.push({
-    //             quantity: 1,
-    //             price_data: {
-    //                 currency: 'USD',
-    //                 product_data: {
-    //                     name: productName,
-    //                 },
-    //                 unit_amount: productPrice * 100,
-    //             },
-    //         });
-    //     }
-
-    //     const stripeSession = await stripe.checkout.sessions.create({
-    //         line_items: stripeLineItems,
-    //         mode: 'payment',
-    //         customer_email: userEmail,
-    //         success_url: process.env.NEXTAUTH_URL + 'orders/' + orderDoc._id.toString() + '?clear-cart=1',
-    //         cancel_url: process.env.NEXTAUTH_URL + 'cart?canceled=1',
-    //         metadata: { orderId: orderDoc._id.toString() },
-    //         payment_intent_data: {
-    //             metadata: { orderId: orderDoc._id.toString() },
-    //         },
-    //         shipping_options: [
-    //             {
-    //                 shipping_rate_data: {
-    //                     display_name: 'Delivery fee',
-    //                     type: 'fixed_amount',
-    //                     fixed_amount: { amount: 500, currency: 'USD' },
-    //                 },
-    //             }
-    //         ],
-    //     });
-
-    //     return Response.json(stripeSession.url);
-    try {
-        await mongoose.connect(process.env.MONGO_URL);
-
-        const { cartProducts, address } = await req.json();
-        const session = await getServerSession(authOptions);
-        const userEmail = session?.user?.email;
-
-        const orderDoc = await Order.create({
-            userEmail,
-            ...address,
-            cartProducts,
-            paid: false,
-        });
-
-        const stripeLineItems = [];
-        for (const cartProduct of cartProducts) {
-
-            const productInfo = await MenuItem.findById(cartProduct._id);
-
-            let productPrice = productInfo.basePrice;
-            if (cartProduct.size) {
-                const size = productInfo.sizes
-                    .find(size => size._id.toString() === cartProduct.size._id.toString());
-                productPrice += size.price;
+        let productPrice = productInfo.basePrice;
+        if (cartProduct.size) {
+            const size = productInfo.sizes
+                .find(size => size._id.toString() === cartProduct.size._id.toString());
+            productPrice += size.price;
+        }
+        if (cartProduct.extras?.length > 0) {
+            for (const cartProductExtraThing of cartProduct.extras) {
+                const productExtras = productInfo.extraIngredientPrices;
+                const extraThingInfo = productExtras
+                    .find(extra => extra._id.toString() === cartProductExtraThing._id.toString());
+                productPrice += extraThingInfo.price;
             }
-            if (cartProduct.extras?.length > 0) {
-                for (const cartProductExtraThing of cartProduct.extras) {
-                    const productExtras = productInfo.extraIngredientPrices;
-                    const extraThingInfo = productExtras
-                        .find(extra => extra._id.toString() === cartProductExtraThing._id.toString());
-                    productPrice += extraThingInfo.price;
-                }
-            }
-
-            const productName = cartProduct.name;
-
-            stripeLineItems.push({
-                quantity: 1,
-                price_data: {
-                    currency: 'USD',
-                    product_data: {
-                        name: productName,
-                    },
-                    unit_amount: productPrice * 100,
-                },
-            });
         }
 
-        const stripeSession = await stripe.checkout.sessions.create({
-            line_items: stripeLineItems,
-            mode: 'payment',
-            customer_email: userEmail,
-            success_url: process.env.NEXTAUTH_URL + 'orders/' + orderDoc._id.toString() + '?clear-cart=1',
-            cancel_url: process.env.NEXTAUTH_URL + 'cart?canceled=1',
-            metadata: { orderId: orderDoc._id.toString() },
-            payment_intent_data: {
-                metadata: { orderId: orderDoc._id.toString() },
-            },
-            shipping_options: [
-                {
-                    shipping_rate_data: {
-                        display_name: 'Delivery fee',
-                        type: 'fixed_amount',
-                        fixed_amount: { amount: 500, currency: 'USD' },
-                    },
-                }
-            ],
-        });
+        const productName = cartProduct.name;
 
-        return Response.json({ sessionId: stripeSession.id });
-    } catch (error) {
-        // Handle errors here
-        console.error(error);
-        return Response.json({ error: 'An error occurred' }, { status: 500 });
-    } finally {
-        // Close MongoDB connection
-        await mongoose.disconnect();
+        stripeLineItems.push({
+            quantity: 1,
+            price_data: {
+                currency: 'USD',
+                product_data: {
+                    name: productName,
+                },
+                unit_amount: productPrice * 100,
+            },
+        });
     }
+
+    const stripeSession = await stripe.checkout.sessions.create({
+        line_items: stripeLineItems,
+        mode: 'payment',
+        customer_email: userEmail,
+        success_url: process.env.NEXTAUTH_URL + 'orders/' + orderDoc._id.toString() + '?clear-cart=1',
+        cancel_url: process.env.NEXTAUTH_URL + 'cart?canceled=1',
+        metadata: { orderId: orderDoc._id.toString() },
+        payment_intent_data: {
+            metadata: { orderId: orderDoc._id.toString() },
+        },
+        shipping_options: [
+            {
+                shipping_rate_data: {
+                    display_name: 'Delivery fee',
+                    type: 'fixed_amount',
+                    fixed_amount: { amount: 500, currency: 'USD' },
+                },
+            }
+        ],
+    });
+
+    return Response.json(stripeSession.url);
+
 }
